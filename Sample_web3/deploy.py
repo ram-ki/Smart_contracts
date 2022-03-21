6659dc567b32b290cfa47f56aed8a2635b604141
ram-ki <46424717+ram-ki@users.noreply.github.com>
2022-03-21 18:50:07 +0530

    Add files via upload

---
+++ Sample_web3/deploy.py
@@ -0,0 +1,106 @@
+import json
+
+from web3 import Web3
+
+# In the video, we forget to `install_solc`
+# from solcx import compile_standard
+from solcx import compile_standard, install_solc
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+# We add these two lines that we forgot from the video!
+print("Installing...")
+install_solc("0.6.0")
+
+# Solidity source code
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get abi
+abi = json.loads(
+    compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
+)["output"]["abi"]
+
+# w3 = Web3(Web3.HTTPProvider(os.getenv("RINKEBY_RPC_URL")))
+# chain_id = 4
+#
+# For connecting to rinkeby testnet, we need to specify the chain id
+# w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+w3 = Web3(Web3.HTTPProvider("https://rinkeby.infura.io/v3/7ae428fa7eb14f239e5431dcf2c570cf"))
+chain_id = 4
+my_address = "0x41A4549B062738aC59A8b7df9B706843036ee1C1"
+private_key = os.getenv("PRIVATE_KEY")
+# private_key = "0xd84c078faa9d31be82a43bdf74c873f1526327f4ef99ed38e6d45729cfb1988f"
+
+print(private_key)
+# Create the contract in Python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+print("created contract : ", SimpleStorage)
+# Get the latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+print("nonce : ", nonce)
+# Submit the transaction that deploys the contract
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+# Sign the transaction
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+print("Deploying Contract!")
+# Send it!
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+# Wait for the transaction to be mined, and get the transaction receipt
+print("Waiting for transaction to finish...")
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
+
+
+# Working with deployed Contracts
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+print(f"Initial Stored Value {simple_storage.functions.retrieve().call()}")
+greeting_transaction = simple_storage.functions.store(15).buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce + 1,
+    }
+)
+signed_greeting_txn = w3.eth.account.sign_transaction(
+    greeting_transaction, private_key=private_key
+)
+tx_greeting_hash = w3.eth.send_raw_transaction(signed_greeting_txn.rawTransaction)
+print("Updating stored Value...")
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_greeting_hash)
+
+print(simple_storage.functions.retrieve().call())
